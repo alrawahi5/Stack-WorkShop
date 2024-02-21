4e128f077a63e85c17de1d9f7dee4ef28bf176cc
ABDULLAH <abdullah555523@hotmail.com>
2024-02-21 10:48:34 +0400

    added a file for stack practic and initilized a stack

---
+++ StackWorkshop.java
@@ -0,0 +1,25 @@
+/*
+* 1- stack decleration.
+*
+* Problem Statement:
+* Write a program to initialize an empty stack.
+* Ensure that your stack can hold up to 10 integers.
+
+(Objective: Familiarize yourself with the basic structure of a stack).
+* */
+
+import java.lang.*;
+import java.util.Stack;
+
+class StackWorkshop{
+    public static void main(String[] args) {
+
+        Stack<Integer> myStack = new Stack<Integer>();
+        System.out.println(myStack.size());
+//
+
+
+
+    }
+
+}
