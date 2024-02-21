5b85e5ccfa85f9503fb22abbe47e791b859ad195
ABDULLAH <abdullah555523@hotmail.com>
2024-02-21 11:51:42 +0400

    tested if the stacks I have are empty

--- StackWorkshop.java
+++ StackWorkshop.java
@@ -18,12 +18,16 @@ class StackWorkshop{
         myStack.push(5);
         myStack.push(8);
         myStack.push(35);
-        
+
         System.out.println("The first element that is removed is: " + myStack.pop());
-
         System.out.println("The secound element that is removed is: " + myStack.pop());
-
         System.out.println("The third element that is removed is: " + myStack.pop());
+
+        Stack<String> myStringStack = new Stack<String>();
+        myStringStack.push("Abdullah");
+
+        System.out.println("The Integer stack is empty: " + " " +  myStack.empty());
+        System.out.println("The String stack is empty: " + " " +  myStringStack.empty() + " and it has " + myStringStack.peek());
 
         System.out.println(myStack.size());
 
