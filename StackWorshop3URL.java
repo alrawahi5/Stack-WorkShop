12faf63d9cc18030a5a72b6760aab64ec31f4891
ABDULLAH <abdullah555523@hotmail.com>
2024-02-25 09:54:19 +0400

    added the URL stack

---
+++ StackWorshop3URL.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+class StackWorkshop3URL{
+    public static void main(String[] args) {
+
+        System.out.println("What URL would you like to vist?: ");
+        Scanner stackScanner = new Scanner(System.in);
+        String URL = stackScanner.nextLine();
+        Stack<String> urlStack = new Stack<String>();
+        while(!URL.equals("back")){
+                urlStack.push(URL);
+                URL = stackScanner.nextLine();
+            }
+            System.out.println("Your browsing history contains the following sites: " + urlStack);
+            System.out.println("The last visited site is: " + urlStack.pop());
+    }
+}
